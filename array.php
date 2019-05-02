a4638dc66459b08d366d696bb64d47c6234c127f
WD Shawon <48361872+shawonahmed01@users.noreply.github.com>
2019-05-02 23:44:31 +0600

    Create array.php

---
+++ array.php
@@ -0,0 +1,98 @@
+<?php 
+
+$number = 12;
+
+$number2 = 1.01;
+
+//+ , - , * , / ,** , ^
+
+//echo $test =1+2;
+
+//B I D M A S
+
+//echo $new = 5**6;
+ 
+ //echo $n = 2**20;
+
+//echo $math = (4+4)/2+3;
+
+  //--$number2;
+
+//echo $number2;
+
+$count = 20;
+
+//echo $count = $count + 30 ;
+
+//echo $count *=700;
+
+//echo floor($number2)
+
+//echo ceil($number2)
+
+//echo pi();
+
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title></title>
+ 	<link rel="stylesheet" href="">
+ </head>
+ <body>
+ 	
+ 	<!.. <h2>array</h2> ..>
+
+ 	<h2>array data</h2>
+
+ 	<?php 
+
+ 	$name ="Shawon ahmed";
+  
+    $place = "DHAK TO ANY WHERE";
+
+    $rel = 'PHP & RONY VAI';
+
+    //$array = array("Shawon ahmed","DHAK TO ANY WHERE","PHP OR RONY VAI");
+
+    ///arry count anything 0 to 9 and more
+
+    //echo $array[0];
+
+
+    //$array = ["Shawon ahmed","DHAK TO ANY WHERE","PHP OR RONY VAI"];
+
+
+    //$array2 = ["new hsawon","DHAK TO ANY barisal","PHP Mentor RONY VAI"];
+    //echo $array [2];
+
+    //$array [0]="WD Shawon";
+
+    //$//array [1]="Chandpur to dhaka";
+
+    //$array[]="html and css";
+
+    //$newarray= array_merge($array,$array2);
+    
+     //echo "<pre>
+     	
+     //";
+    //print_r($newarray)
+
+    $associativearray = array('name'=> 'Shawon',
+     'age'=> '21', 
+     'email'=>'hossainshawon820@gmail.com');
+
+   echo $associativearray['email']
+
+
+
+
+
+ 	 ?>
+ 	
+ </body>
+ </html>
